064010f9f710b6f5511effd8c97e8406bce87d21
Addam Reis <addamreis@gmail.com>
2021-04-28 19:23:39 -0300

    Criação do Projeto (First Commit)

---
+++ EstudosDeCasos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EstudosDeCasos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
